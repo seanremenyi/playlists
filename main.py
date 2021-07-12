49937858ecea934520e698216e99ff02166ead11
Sean Remenyi <sean.remenyi@gmail.com>
2021-07-12 20:44:48 +1000

    step 2 completed

--- main.py
+++ main.py
@@ -1,5 +1,19 @@
 from bs4 import BeautifulSoup
 import requests
+import spotipy
+from spotipy.oauth2 import SpotifyOAuth
+
+sp = spotipy.Spotify(
+    auth_manager=SpotifyOAuth(
+        scope="playlist-modify-private",
+        redirect_uri="http://example.com",
+        client_id=xx,
+        client_secret=xx,
+        show_dialog=True,
+        cache_path="token.txt"
+    )
+)
+user_id = sp.current_user()["id"]
 
 print("Welcome to the time travelling playlist app")
 date = input("Pick a date, any date, type in the format YYYY-MM-DD")
