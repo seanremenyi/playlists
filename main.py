d012a48dd9c8e428a8b5855ace92926c057dbb4c
Sean Remenyi <sean.remenyi@gmail.com>
2021-07-12 21:15:41 +1000

    beautfy

--- main.py
+++ main.py
@@ -4,8 +4,13 @@ import spotipy
 from spotipy.oauth2 import SpotifyOAuth
 
 
-print("Welcome to the time travelling playlist app")
-date = input("Pick a date, any date, type in the format YYYY-MM-DD")
+print("""  __  .__                        ___.                  __           __  .__                          
+_/  |_|  |_________  ______  _  _\_ |__ _____    ____ |  | __     _/  |_|  |__  __ _________  ______ 
+\   __\  |  \_  __ \/  _ \ \/ \/ /| __ \\__  \ _/ ___\|  |/ /     \   __\  |  \|  |  \_  __ \/  ___/ 
+ |  | |   Y  \  | \(  <_> )     / | \_\ \/ __ \\  \___|    <       |  | |   Y  \  |  /|  | \/\___ \  
+ |__| |___|  /__|   \____/ \/\_/  |___  (____  /\___  >__|_ \      |__| |___|  /____/ |__|  /____  > 
+           \/                         \/     \/     \/     \/                \/                  \/""")
+date = input("Pick a date, any date, type in the format YYYY-MM-DD\n")
 response = requests.get("https://www.billboard.com/charts/hot-100/" + date)
 soup = BeautifulSoup(response.text, "html.parser")
 song_names_pull = soup.find_all("span", class_="chart-element__information__song")
@@ -23,20 +28,15 @@ sp = spotipy.Spotify(
 )
 user_id = sp.current_user()["id"]
 
-
-
 song_uris = []
 year = date.split("-")[0]
 for song in song_names:
     result = sp.search(q=f"track:{song} year:{year}", type="track")
-    print(result)
     try:
         uri = result["tracks"]["items"][0]["uri"]
         song_uris.append(uri)
     except IndexError:
-        print(f"{song} doesn't exist in Spotify. Skipped.")
 
 playlist = sp.user_playlist_create(user=user_id, name=f"{date} Throwback Thursday", public=False)
-print(playlist)
 
 sp.playlist_add_items(playlist_id=playlist["id"], items=song_uris)
